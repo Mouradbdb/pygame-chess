c86551d3001539033d5edd279fb30f74468737d3
Mouradbdb <beddibimourad@gmail.com>
2023-03-04 03:20:21 +0100

    added dragger

--- src/main.py
+++ src/main.py
@@ -1,43 +1,58 @@
-import pygame,sys
+import pygame
+import sys
 from const import *
 from game import GAME
+
 
 class MAIN:
 
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((WIDTH,HEIGHT))
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption('chess')
         self.game = GAME()
-        self.dragger = self.game.dragger
-
 
     def mainloop(self):
         game = self.game
         screen = self.screen
+        dragger = self.game.dragger
+        board = self.game.board
         while True:
             game.show_bg(screen)
             game.show_pieces(screen)
+            if dragger.dragging:
+                dragger.update_blit(screen)
+            
             for event in pygame.event.get():
+                # click
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    pass
-                
-                elif event.type == pygame.MOUSEMOTION:
-                    pass
+                    dragger.update_mouse(event.pos)
+                    clicked_row = dragger.mouseY//SQSIZE
+                    clicked_col = dragger.mouseX//SQSIZE
+                    # if clicked square has a piece
+                    if board.squares[clicked_row][clicked_col].has_piece():
+                        piece = board.squares[clicked_row][clicked_col].piece
+                        dragger.save_initial(event.pos)
+                        dragger.drag_piece(piece)
 
-                #click release
+                # motion
+                elif event.type == pygame.MOUSEMOTION:
+                    if dragger.dragging:
+                        dragger.update_mouse(event.pos)
+                        game.show_bg(screen)
+                        game.show_pieces(screen)
+                        dragger.update_blit(screen)
+
+                # click release
                 elif event.type == pygame.MOUSEBUTTONUP:
-                    pass
-                #quite
+                    dragger.undrag_piece()
+                # quite
                 elif event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
 
-
-
-
-            
             pygame.display.update()
+
 
 main = MAIN()
 main.mainloop()
